23ee9359fd3615f86d8f36490be7ddc619f8555e
Parth23-ind <54305480+Parth23-ind@users.noreply.github.com>
2019-08-22 19:57:32 +0530

    Create Leadpageservlet.java

---
+++ Leadpageservlet.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+import java.io.Printwriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import.javax.servlet.http.HttpServletRequest;
+import.javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Leadpageservlet")
+    public class Leadpageservlet extends HttpServlet{
+        Protected void(HttpServletRequest req , HttpServletResponse res) throws ServletException,IOException
+    }
